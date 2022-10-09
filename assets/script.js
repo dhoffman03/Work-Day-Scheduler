dcc1e6b21208913abf49b3e3146ae7776e3f8f60
Dymond <dymond.hoffman03@gmail.com>
2022-10-08 21:08:15 -0700

    time-blocks and custom css added

---
+++ assets/script.js
@@ -0,0 +1,25 @@
+// WHEN I open the planner
+// THEN the current day is displayed at the top of the calendar
+
+
+
+// WHEN I scroll down
+// THEN I am presented with timeblocks for standard business hours
+
+
+
+// WHEN I view the timeblocks for that day
+// THEN each timeblock is color coded to indicate whether it is in the past, present, or future
+
+
+
+
+// WHEN I click the save button for that timeblock
+// THEN the text for that event is saved in local storage
+
+
+
+// WHEN I refresh the page
+// THEN the saved events persist
+//local storage
+
